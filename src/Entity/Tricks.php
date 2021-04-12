f1eeb16d47f06c4ad3ecdec6c38a64fa4416da91
unknown <vallieraxel@gmail.com>
2021-04-12 16:10:10 +0200

    Add Tricks and Groups entities

---
+++ src/Entity/Tricks.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TricksRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TricksRepository::class)
+ */
+class Tricks
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Groups::class, inversedBy="tricks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $group_trick;
+
+    /**
+     * @ORM\Column(type="array", nullable=true)
+     */
+    private $video_urls = [];
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updated_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Users::class)
+     */
+    private $updated_by;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Users::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $created_by;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getGroupTrick(): ?Groups
+    {
+        return $this->group_trick;
+    }
+
+    public function setGroupTrick(?Groups $group_trick): self
+    {
+        $this->group_trick = $group_trick;
+
+        return $this;
+    }
+
+    public function getVideoUrls(): ?array
+    {
+        return $this->video_urls;
+    }
+
+    public function setVideoUrls(?array $video_urls): self
+    {
+        $this->video_urls = $video_urls;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedBy(): ?Users
+    {
+        return $this->updated_by;
+    }
+
+    public function setUpdatedBy(?Users $updated_by): self
+    {
+        $this->updated_by = $updated_by;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?Users
+    {
+        return $this->created_by;
+    }
+
+    public function setCreatedBy(?Users $created_by): self
+    {
+        $this->created_by = $created_by;
+
+        return $this;
+    }
+}
