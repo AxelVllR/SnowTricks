421dae714cb0c26c3d150605c5d842ea62548e70
AxelVllR <vallieraxel@gmail.com>
2021-04-28 11:06:59 +0200

    fix migrations bug

---
+++ migrations/Version20210428090640.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210428090640 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, trick_id INT NOT NULL, user_id INT NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_5F9E962AB281BE2E (trick_id), INDEX IDX_5F9E962AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE groups (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, color VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tricks (id INT AUTO_INCREMENT NOT NULL, group_trick_id INT NOT NULL, updated_by_id INT DEFAULT NULL, created_by_id INT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, updated_at DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, slug VARCHAR(255) NOT NULL, INDEX IDX_E1D902C1BBB1F251 (group_trick_id), INDEX IDX_E1D902C1896DBBDE (updated_by_id), INDEX IDX_E1D902C1B03A8386 (created_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tricks_pictures (id INT AUTO_INCREMENT NOT NULL, tricks_id INT NOT NULL, filename VARCHAR(255) NOT NULL, is_primary TINYINT(1) DEFAULT NULL, INDEX IDX_AF66F4673B153154 (tricks_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, filename VARCHAR(255) NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, pseudo VARCHAR(255) NOT NULL, updated_at DATETIME DEFAULT NULL, token VARCHAR(255) DEFAULT NULL, is_activated TINYINT(1) DEFAULT NULL, UNIQUE INDEX UNIQ_1483A5E9E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, trick_id INT DEFAULT NULL, url VARCHAR(255) NOT NULL, INDEX IDX_7CC7DA2CB281BE2E (trick_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962AB281BE2E FOREIGN KEY (trick_id) REFERENCES tricks (id)');
+        $this->addSql('ALTER TABLE comments ADD CONSTRAINT FK_5F9E962AA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE tricks ADD CONSTRAINT FK_E1D902C1BBB1F251 FOREIGN KEY (group_trick_id) REFERENCES groups (id)');
+        $this->addSql('ALTER TABLE tricks ADD CONSTRAINT FK_E1D902C1896DBBDE FOREIGN KEY (updated_by_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE tricks ADD CONSTRAINT FK_E1D902C1B03A8386 FOREIGN KEY (created_by_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE tricks_pictures ADD CONSTRAINT FK_AF66F4673B153154 FOREIGN KEY (tricks_id) REFERENCES tricks (id)');
+        $this->addSql('ALTER TABLE video ADD CONSTRAINT FK_7CC7DA2CB281BE2E FOREIGN KEY (trick_id) REFERENCES tricks (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tricks DROP FOREIGN KEY FK_E1D902C1BBB1F251');
+        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962AB281BE2E');
+        $this->addSql('ALTER TABLE tricks_pictures DROP FOREIGN KEY FK_AF66F4673B153154');
+        $this->addSql('ALTER TABLE video DROP FOREIGN KEY FK_7CC7DA2CB281BE2E');
+        $this->addSql('ALTER TABLE comments DROP FOREIGN KEY FK_5F9E962AA76ED395');
+        $this->addSql('ALTER TABLE tricks DROP FOREIGN KEY FK_E1D902C1896DBBDE');
+        $this->addSql('ALTER TABLE tricks DROP FOREIGN KEY FK_E1D902C1B03A8386');
+        $this->addSql('DROP TABLE comments');
+        $this->addSql('DROP TABLE groups');
+        $this->addSql('DROP TABLE tricks');
+        $this->addSql('DROP TABLE tricks_pictures');
+        $this->addSql('DROP TABLE users');
+        $this->addSql('DROP TABLE video');
+    }
+}
